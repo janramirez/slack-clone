2a84b685adae979df303510d8db2403a117aa3b4
janramirez <jramirez.ust@gmail.com>
2022-10-21 00:01:26 +0800

    Added new machine

--- src/App.js
+++ src/App.js
@@ -5,23 +5,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Chat from './Chat';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
       <Router>
+      {!user ? (
+            <h1>LOGIN page</h1>
+          ): (
+        <>
+        < Header />
 
-      < Header />
+        <div className='app__body'>
+          < Sidebar />
+          < Routes >
+            
+            <Route path="/room/:roomId" element={<Chat />} />
 
-      <div className='app__body'>
-        < Sidebar />
-        < Routes >
-          <Route path="/room/:roomId" element={<Chat />} />
-
-          <Route path="/" element={<h1>No Channel selected</h1>} />
-          {/* <Route path='/room/:roomId'>
-            <h1>Channel</h1>
-          </Route> */}
-        </Routes>
-      </div>
+            <Route path="/" element={<h1>No Channel selected</h1>} />
+            {/* <Route path='/room/:roomId'>
+              <h1>Channel</h1>
+            </Route> */}
+          </Routes>
+        </div>
+        </>             
+      )}
 
       </Router>
 
--- src/Message.js
+++ src/Message.js
@@ -6,9 +6,12 @@ function Message({ message, timestamp, user, userImage }) {
     <div className='message'>
         <img src={userImage} alt='' />
         <div className='message__info'>
-            <h4>{user} <span className='message__timestamp'>
+            <h4>
+              {user}{" "} 
+              <span className='message__timestamp'>
             {new Date(timestamp?.toDate()).toUTCString()}
-                </span></h4>
+              </span>
+            </h4>
             <p>{message}</p>
         </div>
     </div>
